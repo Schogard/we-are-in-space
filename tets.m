f0a88f38e3c25cfb53f5e80bf993dcd4112f6e98
Schogard <dragossirghi@yahoo.ro>
2024-10-14 17:44:12 +0200

    MATLAB WORKS

--- tets.m
+++ tets.m
@@ -1,10 +1,10 @@
 
-Corners=[156,315;283,297;339,270;31,30];
+Corners=[1, 5;1, 2;3, 0;6, 6];
 %function to order corners
 function [A,B,C,D] = orderCroners(Corners)
 %must find corner A by sorting the Corners over the smallest distance to
 %0,0, that is to say smallest x2+y2
-distances=Corners(:,1).^2+Corners(:,2).^2
+distances=Corners(:,1).^2 + Corners(:,2).^2
 [distances, index_distances]=sort(distances)
 xA=Corners(index_distances(1), 1);
 yA=Corners(index_distances(1), 2);
@@ -15,6 +15,7 @@ Corners(index_distances(1), :)=aux
 Corners(1, :)=A
 %now we must order the B C D such that atan((y-yA)/(x-xA)) is minimal
 angles=atan2((Corners(:, 2)-yA), (Corners(:, 1)-xA))
+angles(1, :)=[] %delete A
 [angles, index_angles]=sort(angles)
 %now the last 3 angles correspond to B C and D
 B=[Corners(index_angles(2), 1), Corners(index_angles(2), 2)];
