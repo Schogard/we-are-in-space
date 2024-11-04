941b4ad789b02a0535db3db33dceef2c0658a4a6
Schogard <dragossirghi@yahoo.ro>
2024-11-04 16:20:00 +0100

    Add files via upload

--- tets.m
+++ tets.m
@@ -1,7 +1,7 @@
 
-Corners=[1, 5;1, 2;3, 0;6, 6];
+Corners=[0, 0;1, 1; 0, 2;6, 6];
 %function to order corners
-function [A,B,C,D] = orderCorners(Corners)
+%function [A,B,C,D] = orderCorners(Corners)
 %must find corner A by sorting the Corners over the smallest distance to
 %0,0, that is to say smallest x2+y2
 distances=Corners(:,1).^2 + Corners(:,2).^2
@@ -15,12 +15,13 @@ Corners(index_distances(1), :)=aux
 Corners(1, :)=A
 %now we must order the B C D such that atan((y-yA)/(x-xA)) is minimal
 angles=atan2((Corners(:, 2)-yA), (Corners(:, 1)-xA))
-angles(1, :)=[] %delete A
 [angles, index_angles]=sort(angles)
+angles(1, :)=[] %delete A
+index_angles(1, :)=[]
 %now the last 3 angles correspond to B C and D
 B=[Corners(index_angles(1), 1), Corners(index_angles(1), 2)];
 C=[Corners(index_angles(2), 1), Corners(index_angles(2), 2)];
 D=[Corners(index_angles(3), 1), Corners(index_angles(3), 2)];
-end
+%end
 
-[A,B,C,D] = orderCorners(Corners)
+%[A,B,C,D] = orderCorners(Corners)
