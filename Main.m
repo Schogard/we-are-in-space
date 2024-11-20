9a9b42dd5b8fab1b3c10f4d815f274a2ee87f2e6
Schogard <dragossirghi@yahoo.ro>
2024-11-20 23:55:16 +0100

    MATLAB final file
    It works well, the pdf must be edited to show title and axis labels

---
+++ Main.m
@@ -0,0 +1,642 @@
+clear
+Corners= [
+99, 310;
+229, 189;
+197, 76;
+21, 29;
+];
+
+C0= [
+117, 291;
+116, 290;
+121, 289;
+114, 288;
+121, 286;
+111, 285;
+122, 283;
+109, 281;
+122, 279;
+106, 276;
+122, 274;
+103, 271;
+121, 268;
+100, 265;
+121, 262;
+97, 258;
+120, 255;
+93, 250;
+119, 247;
+89, 242;
+118, 239;
+173, 239;
+174, 239;
+172, 238;
+175, 238;
+171, 236;
+175, 236;
+170, 234;
+175, 234;
+85, 233;
+117, 231;
+168, 231;
+175, 231;
+167, 228;
+175, 228;
+165, 224;
+175, 224;
+81, 223;
+116, 222;
+174, 220;
+163, 219;
+173, 215;
+77, 214;
+161, 214;
+115, 213;
+205, 210;
+173, 209;
+204, 209;
+206, 209;
+158, 208;
+203, 208;
+206, 208;
+206, 207;
+202, 206;
+206, 205;
+113, 204;
+172, 204;
+201, 204;
+72, 203;
+156, 202;
+205, 202;
+200, 201;
+205, 199;
+171, 198;
+199, 198;
+153, 196;
+204, 196;
+112, 194;
+197, 194;
+68, 193;
+204, 192;
+225, 192;
+169, 191;
+224, 191;
+225, 191;
+196, 190;
+224, 190;
+225, 190;
+151, 189;
+225, 189;
+203, 188;
+223, 188;
+225, 187;
+222, 186;
+168, 185;
+194, 185;
+225, 185;
+110, 184;
+221, 184;
+202, 183;
+225, 183;
+148, 182;
+192, 181;
+220, 181;
+224, 180;
+201, 179;
+167, 178;
+219, 178;
+223, 177;
+190, 176;
+109, 175;
+145, 175;
+218, 175;
+200, 174;
+223, 174;
+166, 171;
+216, 171;
+188, 170;
+222, 170;
+199, 168;
+142, 167;
+215, 167;
+221, 166;
+107, 165;
+186, 165;
+164, 164;
+198, 163;
+214, 163;
+220, 162;
+139, 159;
+184, 159;
+212, 159;
+197, 158;
+219, 158;
+163, 157;
+105, 155;
+210, 154;
+182, 153;
+136, 152;
+196, 152;
+162, 150;
+209, 149;
+180, 147;
+195, 147;
+104, 146;
+207, 145;
+133, 144;
+160, 143;
+178, 141;
+194, 141;
+206, 140;
+102, 137;
+130, 136;
+159, 136;
+176, 135;
+192, 135;
+204, 135;
+191, 130;
+202, 130;
+127, 129;
+158, 129;
+174, 129;
+101, 128;
+190, 125;
+201, 125;
+172, 124;
+156, 123;
+125, 121;
+199, 120;
+100, 119;
+189, 119;
+170, 118;
+155, 116;
+197, 116;
+188, 115;
+122, 114;
+168, 112;
+99, 111;
+196, 111;
+154, 110;
+187, 110;
+119, 107;
+166, 107;
+194, 107;
+153, 105;
+186, 105;
+97, 104;
+193, 103;
+164, 102;
+185, 101;
+116, 100;
+152, 100;
+191, 99;
+97, 97;
+162, 97;
+185, 97;
+190, 96;
+152, 95;
+114, 94;
+184, 94;
+160, 93;
+189, 92;
+184, 91;
+96, 90;
+151, 90;
+188, 90;
+159, 89;
+111, 88;
+183, 88;
+187, 87;
+150, 86;
+183, 86;
+95, 85;
+157, 85;
+186, 85;
+183, 84;
+109, 83;
+150, 83;
+185, 83;
+156, 82;
+183, 82;
+184, 82;
+183, 81;
+184, 81;
+150, 80;
+154, 80;
+95, 79;
+107, 78;
+150, 78;
+153, 77;
+150, 76;
+152, 76;
+95, 75;
+150, 75;
+152, 75;
+105, 74;
+150, 74;
+151, 74;
+94, 71;
+103, 71;
+95, 68;
+101, 68;
+95, 66;
+100, 66;
+95, 64;
+98, 64;
+96, 63;
+97, 63;
+];
+
+C1= [
+85, 256;
+86, 252;
+86, 248;
+87, 244;
+87, 241;
+88, 237;
+89, 234;
+89, 231;
+90, 227;
+90, 224;
+91, 222;
+92, 219;
+92, 216;
+93, 214;
+94, 211;
+95, 209;
+95, 207;
+96, 204;
+97, 202;
+98, 200;
+98, 199;
+99, 197;
+100, 195;
+101, 194;
+182, 193;
+183, 193;
+184, 193;
+185, 193;
+186, 193;
+187, 193;
+188, 193;
+189, 193;
+190, 193;
+191, 193;
+102, 192;
+178, 192;
+179, 192;
+180, 192;
+181, 192;
+191, 192;
+192, 192;
+193, 192;
+102, 191;
+176, 191;
+177, 191;
+178, 191;
+194, 191;
+173, 190;
+174, 190;
+175, 190;
+195, 190;
+103, 189;
+171, 189;
+172, 189;
+173, 189;
+196, 189;
+197, 189;
+104, 188;
+169, 188;
+170, 188;
+197, 188;
+198, 188;
+105, 187;
+167, 187;
+168, 187;
+198, 187;
+165, 186;
+166, 186;
+198, 186;
+199, 186;
+106, 185;
+162, 185;
+163, 185;
+164, 185;
+199, 185;
+107, 184;
+161, 184;
+162, 184;
+200, 184;
+108, 183;
+158, 183;
+159, 183;
+160, 183;
+200, 183;
+108, 182;
+157, 182;
+158, 182;
+200, 182;
+201, 182;
+109, 181;
+110, 181;
+154, 181;
+155, 181;
+156, 181;
+201, 181;
+111, 180;
+153, 180;
+201, 180;
+112, 179;
+150, 179;
+151, 179;
+152, 179;
+113, 178;
+114, 178;
+147, 178;
+148, 178;
+149, 178;
+202, 178;
+115, 177;
+116, 177;
+145, 177;
+146, 177;
+147, 177;
+202, 177;
+117, 176;
+142, 176;
+143, 176;
+144, 176;
+202, 176;
+118, 175;
+119, 175;
+120, 175;
+139, 175;
+140, 175;
+141, 175;
+202, 175;
+121, 174;
+122, 174;
+123, 174;
+124, 174;
+125, 174;
+133, 174;
+134, 174;
+135, 174;
+136, 174;
+137, 174;
+138, 174;
+202, 174;
+203, 174;
+126, 173;
+127, 173;
+128, 173;
+129, 173;
+130, 173;
+131, 173;
+132, 173;
+203, 173;
+203, 172;
+203, 171;
+203, 170;
+203, 169;
+203, 168;
+203, 167;
+204, 166;
+204, 165;
+204, 164;
+204, 163;
+204, 162;
+204, 161;
+204, 160;
+204, 159;
+204, 158;
+204, 157;
+204, 156;
+204, 154;
+204, 153;
+204, 152;
+204, 151;
+204, 150;
+204, 148;
+204, 147;
+204, 146;
+204, 144;
+204, 143;
+204, 142;
+204, 140;
+204, 139;
+204, 138;
+204, 136;
+204, 133;
+203, 132;
+203, 131;
+203, 129;
+203, 128;
+203, 126;
+203, 125;
+203, 123;
+203, 121;
+202, 120;
+202, 118;
+202, 117;
+202, 115;
+202, 113;
+202, 112;
+202, 110;
+201, 108;
+201, 107;
+201, 105;
+201, 103;
+201, 102;
+200, 100;
+200, 98;
+200, 96;
+200, 95;
+200, 93;
+199, 91;
+199, 89;
+199, 87;
+199, 85;
+198, 84;
+198, 82;
+];
+
+T ={C0 C1 };
+
+
+
+
+
+
+% Main function
+%function Main(filename, Curve, Corners)
+    % get width and height of image
+    Width=max(Corners(:, 1))
+    Height=max(Corners(:, 2))
+    leg=[] %legend to construct
+    f=figure, %figure to draw
+
+    % 1. Step: Order Corners
+    [A,B,C,D] = orderCorners(Corners);
+
+    % 2. Step: Calculate Matrix
+    M = computeM(A, B, C, D, Width, Height);
+
+    for i = 1:length(T) %for all traces
+        Curves=cell2mat(T(i));
+        X = Curves(:,1);
+        Y = Curves(:,2);
+
+        % 3. Step: Apply M
+        [x y] = applyM(M, X, Y);
+
+        % 4. Step: Fit curve
+        [ParamStr yfitted] = FindTrace(x, y, Width, Height);
+
+        %Step 5, plotting
+        %plot fitted line (data needs to be sorted by x so it can fit the
+        %lines)
+        %resize the original data so it fits on the graph (W=1s, H=2v)
+        xresized=x./Width
+        yresized=y./(0.5*Height)-1
+        [xsort, index]=sort(xresized)
+        ysort=yfitted(index)
+        plot(xsort, ysort);
+        %construct the legend
+        leg=[leg ParamStr]
+        hold on;
+    end
+    %output the legend
+    lgd=legend(leg)
+    lgd.AutoUpdate ="off";
+    %6. Step plot original data
+    for i = 1:length(T) %for all traces
+        Curves=cell2mat(T(i));
+        X = Curves(:,1); 
+        Y = Curves(:,2);
+        [x y] = applyM(M, X, Y);
+        %resize the original data so it fits on the graph (W=1s, H=2v)
+        xresized=x./Width
+        yresized=y./(0.5*Height)-1
+        %plot original data points
+        plot(xresized, yresized, 'o');
+        hold on;
+    end
+    hold off;
+
+    %7. Step pdf file
+    saveas(f,'myfigure.pdf')
+    %TODO, title of figure, axis labeling, main axis highlighted
+%end
+%end of main function
+
+
+% function to apply M to "untangle" the coordinates to homogenuous coordinates
+function [x y] = applyM(M, X, Y) 
+    point = [X Y]     
+    point(:, end+1)=1       % homogenuous coordinates
+    newpoint= M*point.';
+    point_transformed=newpoint.';
+    x=point_transformed(:, 1)./point_transformed(:, 3)
+    y=point_transformed(:, 2)./point_transformed(:, 3)
+end
+
+
+
+% function to compute matrix M
+function M = computeM(A,B,C, D, Width, Height)
+    %corner A goes to 0 0
+    %corner B goes to W 0
+    %corner C goes to W H
+    %corner D goes to 0 H
+    matrix=[A(1), A(2), 1, 0, 0, 0, -A(1)*0, -A(2)*0;
+            0, 0, 0, A(1), A(2), 1, -A(1)*0, -A(2)*0;
+            B(1), B(2), 1, 0, 0, 0, -B(1)*Width, -B(2)*Width;
+            0, 0, 0, B(1), B(2), 1, -B(1)*0, -B(2)*0;
+            C(1), C(2), 1, 0, 0, 0, -C(1)*Width, -C(2)*Width;
+            0, 0, 0, C(1), C(2), 1, -C(1)*Height, -C(2)*Height;
+            D(1), D(2), 1, 0, 0, 0, -D(1)*0, -D(2)*0;
+            0, 0, 0, D(1), D(2), 1, -D(1)*Height, -D(2)*Height;]
+    b=[0; 0; Width; 0; Width; Height; 0; Height]
+    
+    % Matrix division Ax=b => x = A\b
+    x=matrix\b 
+    M=[x(1), x(2), x(3);
+        x(4), x(5), x(6);
+        x(7), x(8), 1
+        ] 
+end
+
+
+
+function  [ParamStr, yfitted] = FindTrace(X, Y, Width, Height)
+    %resize the data such that width is 1s and height is 2v
+    X=X./(Width)
+    Y=Y./(Height*0.5)-1
+    %plot(X, Y, 'ob'); DEBUG
+    %hold on;
+    %try plotting a sine
+    %to find frequency, approximate as a 12deg polyn
+    [Xsort, index]=sort(X)
+    Ysort=Y(index)
+    polyn = polyfit(Xsort,Ysort,20);
+    %find the points where the polyn is mean(Y)
+    polyn(end)=polyn(end)-mean(Y)
+    sols=roots(polyn)
+    Xmax=max(X);
+    sols=sols(find(0<sols & sols<Xmax & conj(sols)==sols)) %check if solutions are in domain and real
+    if length(sols)<3 & 1<length(sols)
+        dist=sols(end)-sols(end-1)
+    elseif length(sols)==1 %this is probably gonna be a polynomial
+            dist=max(X)-min(X)
+    else dist=sols(end-1)-sols(end-2)
+    end
+
+    opts = fitoptions('Method','NonlinearLeastSquares');
+    ft=fittype('a*sin(b*x+c)+d');
+    [sortY, index]=sort(Y, 'descend');
+    omega=dist
+    opts.StartPoint=[abs((max(Y)-min(Y))/2), pi/abs(omega), Y(1), mean(Y)]
+    [fitResult, gofsine, output] = fit(X, Y, ft, opts );
+    %plot(fitResult); DEBUG
+    %try plotting a 4 degree polynomial
+    [polyn4, gofpolyn] = fit(Xsort,Ysort,'poly4');
+    %check the gofs and pick the lowest one
+    if gofpolyn.rmse< gofsine.rmse
+        %output polynomial coefficients
+        ParamStr=sprintf("a4: %.2f, a3: %.2f, a2: %.2f, a1: %.2f, RMSE: %.2f", polyn4(1), polyn4(2), polyn4(3), polyn4(4), gofpolyn.rmse)
+        yfitted=polyn4(X);
+    else
+        %output afpo of sine in paramstr
+        ParamStr=sprintf("A: %.2f[v], DC: %.2f[v], F: %.2f[Hz], P: %.2f [deg], RMSE: %.2f", fitResult.a, fitResult.d, fitResult.b/(2*pi), rad2deg(fitResult.c), gofsine.rmse)
+        yfitted=fitResult(X);
+    end
+end
+
+
+%function to order corners
+function [A,B,C,D] = orderCorners(Corners)
+%must find corner A by sorting the Corners over the smallest distance to
+%0,0, that is to say smallest x2+y2
+distances=Corners(:,1).^2 + Corners(:,2).^2
+[distances, index_distances]=sort(distances)
+xA=Corners(index_distances(1), 1);
+yA=Corners(index_distances(1), 2);
+A=[xA, yA];
+%put A at the beginning of Corners
+aux=[Corners(1, 1), Corners(1, 2)]
+Corners(index_distances(1), :)=aux
+Corners(1, :)=A
+%now we must order the B C D such that atan((y-yA)/(x-xA)) is minimal
+angles=atan2((Corners(:, 2)-yA),(Corners(:, 1)-xA))
+angles(1, :)=[] %delete A
+%we copy corners and we delete A from it as well
+CornersCopy=Corners;
+CornersCopy(1, :)=[] %delete A
+[angles, index_angles]=sort(angles)
+%now the last 3 angles correspond to B C and D
+B=[CornersCopy(index_angles(1), 1), CornersCopy(index_angles(1), 2)];
+C=[CornersCopy(index_angles(2), 1), CornersCopy(index_angles(2), 2)];
+D=[CornersCopy(index_angles(3), 1), CornersCopy(index_angles(3), 2)];
+end
+   
