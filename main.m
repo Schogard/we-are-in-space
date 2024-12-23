3a39dbdb24deabf49c06c555be3a9471c1e147be
Bigdag <theo.maechler@epfl.ch>
2024-12-23 13:26:43 +0100

    Add files via upload
    Need to change colours array, but outputs each color

---
+++ main.m
@@ -0,0 +1,827 @@
+clear
+Corners= [
+156, 315;
+283, 297;
+339, 270;
+31, 30;
+];
+
+C0= [
+190, 309;
+191, 309;
+192, 309;
+193, 309;
+194, 309;
+185, 308;
+186, 308;
+187, 308;
+189, 308;
+199, 308;
+200, 308;
+201, 308;
+183, 307;
+184, 307;
+205, 307;
+206, 307;
+180, 306;
+181, 306;
+207, 306;
+208, 306;
+210, 306;
+179, 305;
+211, 305;
+212, 305;
+175, 304;
+177, 304;
+214, 304;
+174, 303;
+215, 303;
+216, 303;
+172, 302;
+218, 302;
+170, 301;
+219, 301;
+221, 301;
+168, 300;
+223, 300;
+166, 299;
+224, 299;
+226, 298;
+271, 298;
+272, 298;
+164, 297;
+228, 297;
+271, 297;
+272, 297;
+275, 297;
+276, 297;
+277, 297;
+161, 296;
+230, 296;
+272, 296;
+273, 296;
+278, 296;
+279, 296;
+280, 296;
+232, 295;
+273, 295;
+274, 295;
+281, 295;
+282, 295;
+283, 295;
+158, 294;
+234, 294;
+274, 294;
+275, 294;
+284, 294;
+285, 294;
+155, 293;
+236, 293;
+275, 293;
+276, 293;
+286, 293;
+287, 293;
+238, 292;
+277, 292;
+278, 292;
+288, 292;
+290, 292;
+152, 291;
+240, 291;
+278, 291;
+291, 291;
+292, 291;
+242, 290;
+279, 290;
+280, 290;
+294, 290;
+295, 290;
+281, 289;
+296, 289;
+148, 288;
+244, 288;
+282, 288;
+298, 288;
+299, 288;
+247, 287;
+283, 287;
+301, 287;
+144, 286;
+249, 286;
+285, 286;
+303, 286;
+304, 286;
+286, 285;
+306, 285;
+252, 284;
+287, 284;
+288, 284;
+307, 284;
+254, 283;
+290, 283;
+257, 282;
+312, 282;
+291, 281;
+259, 280;
+292, 280;
+317, 280;
+262, 279;
+294, 279;
+319, 279;
+295, 278;
+265, 277;
+297, 277;
+268, 276;
+298, 276;
+300, 275;
+270, 274;
+301, 274;
+273, 273;
+303, 272;
+276, 271;
+304, 271;
+279, 270;
+306, 270;
+307, 269;
+282, 268;
+285, 267;
+309, 267;
+287, 266;
+310, 266;
+311, 265;
+290, 264;
+313, 264;
+293, 263;
+314, 263;
+296, 262;
+315, 262;
+298, 260;
+316, 260;
+301, 259;
+317, 259;
+303, 258;
+318, 258;
+305, 257;
+308, 257;
+319, 257;
+309, 256;
+319, 256;
+311, 255;
+313, 255;
+319, 255;
+314, 254;
+316, 254;
+317, 254;
+];
+
+C1= [
+152, 304;
+154, 304;
+156, 304;
+158, 303;
+160, 303;
+162, 302;
+164, 301;
+166, 300;
+168, 299;
+170, 298;
+233, 297;
+234, 297;
+235, 297;
+236, 297;
+237, 297;
+238, 297;
+172, 296;
+233, 296;
+239, 296;
+240, 296;
+174, 295;
+241, 295;
+242, 295;
+232, 294;
+244, 294;
+246, 294;
+271, 294;
+272, 294;
+176, 293;
+232, 293;
+247, 293;
+271, 293;
+273, 293;
+274, 293;
+232, 292;
+249, 292;
+271, 292;
+272, 292;
+276, 292;
+291, 292;
+292, 292;
+179, 291;
+232, 291;
+251, 291;
+273, 291;
+279, 291;
+280, 291;
+293, 291;
+294, 291;
+232, 290;
+253, 290;
+273, 290;
+274, 290;
+281, 290;
+293, 290;
+296, 290;
+181, 289;
+233, 289;
+255, 289;
+275, 289;
+283, 289;
+285, 289;
+294, 289;
+295, 289;
+257, 288;
+276, 288;
+286, 288;
+296, 288;
+297, 288;
+183, 287;
+233, 287;
+259, 287;
+277, 287;
+288, 287;
+298, 287;
+233, 286;
+278, 286;
+290, 286;
+299, 286;
+186, 285;
+262, 285;
+279, 285;
+292, 285;
+294, 285;
+301, 285;
+302, 285;
+234, 284;
+264, 284;
+281, 284;
+296, 284;
+303, 284;
+188, 283;
+234, 283;
+266, 283;
+282, 283;
+297, 283;
+304, 283;
+269, 282;
+283, 282;
+299, 282;
+305, 282;
+307, 282;
+191, 281;
+234, 281;
+271, 281;
+284, 281;
+301, 281;
+303, 281;
+308, 281;
+285, 280;
+304, 280;
+309, 280;
+310, 280;
+194, 279;
+235, 279;
+274, 279;
+286, 279;
+306, 279;
+307, 279;
+310, 279;
+311, 279;
+276, 278;
+287, 278;
+309, 278;
+310, 278;
+311, 278;
+312, 278;
+197, 277;
+235, 277;
+278, 277;
+288, 277;
+311, 277;
+312, 277;
+280, 276;
+282, 276;
+289, 276;
+235, 275;
+284, 275;
+290, 275;
+200, 274;
+286, 274;
+287, 274;
+290, 274;
+235, 273;
+288, 273;
+289, 273;
+290, 273;
+203, 272;
+235, 272;
+206, 270;
+235, 270;
+209, 268;
+235, 268;
+234, 267;
+212, 266;
+215, 265;
+233, 265;
+218, 264;
+232, 264;
+221, 263;
+231, 263;
+223, 262;
+225, 262;
+228, 262;
+229, 262;
+];
+
+C2= [
+148, 295;
+150, 295;
+153, 294;
+158, 292;
+206, 292;
+207, 292;
+208, 292;
+210, 292;
+211, 292;
+160, 291;
+205, 291;
+213, 291;
+215, 291;
+242, 291;
+243, 291;
+244, 291;
+204, 290;
+217, 290;
+241, 290;
+245, 290;
+246, 290;
+264, 290;
+265, 290;
+266, 290;
+162, 289;
+203, 289;
+219, 289;
+241, 289;
+248, 289;
+264, 289;
+267, 289;
+268, 289;
+279, 289;
+280, 289;
+282, 289;
+290, 289;
+291, 289;
+298, 289;
+165, 288;
+202, 288;
+221, 288;
+241, 288;
+250, 288;
+252, 288;
+264, 288;
+269, 288;
+271, 288;
+279, 288;
+280, 288;
+283, 288;
+284, 288;
+290, 288;
+291, 288;
+292, 288;
+293, 288;
+294, 288;
+300, 288;
+201, 287;
+241, 287;
+254, 287;
+265, 287;
+273, 287;
+281, 287;
+286, 287;
+292, 287;
+295, 287;
+296, 287;
+300, 287;
+302, 287;
+167, 286;
+223, 286;
+241, 286;
+256, 286;
+266, 286;
+274, 286;
+282, 286;
+287, 286;
+289, 286;
+292, 286;
+293, 286;
+298, 286;
+301, 286;
+302, 286;
+200, 285;
+226, 285;
+242, 285;
+258, 285;
+267, 285;
+276, 285;
+282, 285;
+283, 285;
+290, 285;
+295, 285;
+299, 285;
+303, 285;
+304, 285;
+169, 284;
+198, 284;
+228, 284;
+242, 284;
+260, 284;
+267, 284;
+278, 284;
+280, 284;
+284, 284;
+302, 284;
+305, 284;
+230, 283;
+242, 283;
+262, 283;
+268, 283;
+281, 283;
+283, 283;
+285, 283;
+296, 283;
+305, 283;
+306, 283;
+172, 282;
+197, 282;
+242, 282;
+263, 282;
+265, 282;
+268, 282;
+284, 282;
+285, 282;
+233, 281;
+235, 281;
+242, 281;
+266, 281;
+267, 281;
+268, 281;
+175, 280;
+196, 280;
+236, 280;
+241, 280;
+177, 279;
+238, 279;
+239, 279;
+180, 278;
+192, 278;
+183, 277;
+188, 277;
+190, 277;
+185, 276;
+];
+
+C3= [
+155, 311;
+157, 311;
+159, 311;
+160, 310;
+162, 310;
+164, 310;
+165, 309;
+167, 309;
+169, 309;
+170, 308;
+172, 308;
+174, 308;
+175, 307;
+177, 307;
+178, 307;
+183, 305;
+185, 305;
+186, 304;
+188, 304;
+189, 303;
+191, 303;
+192, 302;
+194, 302;
+195, 301;
+197, 301;
+199, 300;
+200, 300;
+202, 299;
+203, 298;
+205, 298;
+207, 297;
+208, 296;
+210, 296;
+212, 295;
+284, 295;
+285, 295;
+286, 295;
+213, 294;
+215, 294;
+283, 294;
+288, 294;
+217, 293;
+283, 293;
+284, 293;
+219, 292;
+220, 292;
+284, 292;
+285, 292;
+222, 291;
+285, 291;
+286, 291;
+224, 290;
+286, 290;
+287, 290;
+226, 289;
+287, 289;
+288, 289;
+227, 288;
+229, 288;
+288, 288;
+289, 288;
+231, 287;
+289, 287;
+290, 287;
+291, 286;
+235, 285;
+291, 285;
+237, 284;
+239, 284;
+292, 284;
+293, 284;
+241, 283;
+293, 283;
+294, 283;
+243, 282;
+294, 282;
+295, 282;
+245, 281;
+295, 281;
+296, 281;
+246, 280;
+248, 280;
+297, 280;
+250, 279;
+298, 279;
+252, 278;
+299, 278;
+254, 277;
+300, 277;
+256, 276;
+258, 276;
+301, 276;
+260, 275;
+302, 275;
+262, 274;
+303, 274;
+264, 273;
+304, 273;
+266, 272;
+268, 272;
+304, 272;
+305, 272;
+270, 271;
+305, 271;
+272, 270;
+273, 270;
+275, 269;
+306, 269;
+277, 268;
+279, 268;
+307, 268;
+281, 267;
+282, 267;
+307, 267;
+284, 266;
+286, 266;
+308, 266;
+287, 265;
+289, 265;
+308, 265;
+292, 264;
+293, 264;
+307, 264;
+294, 263;
+295, 263;
+297, 263;
+298, 263;
+305, 263;
+306, 263;
+307, 263;
+299, 262;
+300, 262;
+301, 262;
+302, 262;
+303, 262;
+304, 262;
+305, 262;
+];
+
+T ={C0 C1 C2 C3 };
+
+filename="image.png" %PLACEHOLDER, TO REMOVE IN THE FINAL VARIANT, will come from labview
+colours=[1 0 0; 0 1 0; 0 0 1; 0 0 0] %TEST
+optionmarker='o'
+
+
+
+% Main function
+%function Main(filename, Curve, Corners)
+    % get width and height of image
+    Width=max(Corners(:, 1))
+    Height=max(Corners(:, 2))
+    legstring=[] %legend to construct
+    f=figure, %figure to draw
+    % 1. Step: Order Corners
+    [A,B,C,D] = orderCorners(Corners);
+
+    % 2. Step: Calculate Matrix
+    M = computeM(A, B, C, D, Width, Height);
+
+    for i = 1:length(T) %for all traces, to calculate the fit and to plot the fit
+        Curves=cell2mat(T(i));
+        X = Curves(:,1);
+        Y = Curves(:,2);
+
+        % 3. Step: Apply M
+        [x y] = applyM(M, X, Y);
+
+        % 4. Step: Fit curve
+        [ParamStr yfitted] = FindTrace(x, y, Width, Height);
+
+        %Step 5, plotting
+        %plot fitted line (data needs to be sorted by x so it can fit the
+        %lines)
+        %resize the original data so it fits on the graph (W=1s, H=2v)
+        xresized=x./Width
+        yresized=y./(0.5*Height)-1
+        %plot the fitted data
+        [xsort, index]=sort(xresized)
+        ysort=yfitted(index)
+        plot(xsort, ysort, 'color', colours(i,:));
+        %construct the legend
+        legstring=[legstring ParamStr]
+        hold on;
+    end
+    %output the legend
+    lgd=legend(legstring)
+    lgd.AutoUpdate ="off";
+    lgd.Location='southoutside'
+    %6. Step plot original data
+    for i = 1:length(T) %for all traces
+        Curves=cell2mat(T(i));
+        X = Curves(:,1); 
+        Y = Curves(:,2);
+        [x y] = applyM(M, X, Y);
+        %resize the original data so it fits on the graph (W=1s, H=2v)
+        xresized=x./Width
+        yresized=y./(0.5*Height)-1
+        %plot original data points
+        plot(xresized, yresized, optionmarker, 'color', colours(i, :));
+        hold on;
+    end
+    hold off;
+
+    %add title
+    [filepath,name,ext] = fileparts(filename)
+    t=datestr(datetime)
+    titre=strcat("SRC : ‘", name, ext, "’ @ ", t)
+    title(titre)
+    %add grid lines and axis labels
+    grid on;
+    xticks(0:0.1:1)
+    yticks([-1:0.2:1])
+    axis([0 1 -1 1])
+    yline(0);
+    xline(0.5);
+    xlabel("0.1[s]/div")
+    ylabel("0.2[v]/div")
+    %7. Step pdf file
+    pdffilename=strcat(name, ".pdf")
+    saveas(f, pdffilename)
+    %TODO axis labeling, main axis highlighted
+%end
+%end of main function
+
+
+% function to apply M to "untangle" the coordinates to homogenuous coordinates
+function [x y] = applyM(M, X, Y) 
+    point = [X Y]     
+    point(:, end+1)=1       % homogenuous coordinates
+    newpoint= M*point.';
+    point_transformed=newpoint.';
+    x=point_transformed(:, 1)./point_transformed(:, 3)
+    y=point_transformed(:, 2)./point_transformed(:, 3)
+end
+
+
+
+% function to compute matrix M
+function M = computeM(A,B,C, D, Width, Height)
+    %corner A goes to 0 0
+    %corner B goes to W 0
+    %corner C goes to W H
+    %corner D goes to 0 H
+    matrix=[A(1), A(2), 1, 0, 0, 0, -A(1)*0, -A(2)*0;
+            0, 0, 0, A(1), A(2), 1, -A(1)*0, -A(2)*0;
+            B(1), B(2), 1, 0, 0, 0, -B(1)*Width, -B(2)*Width;
+            0, 0, 0, B(1), B(2), 1, -B(1)*0, -B(2)*0;
+            C(1), C(2), 1, 0, 0, 0, -C(1)*Width, -C(2)*Width;
+            0, 0, 0, C(1), C(2), 1, -C(1)*Height, -C(2)*Height;
+            D(1), D(2), 1, 0, 0, 0, -D(1)*0, -D(2)*0;
+            0, 0, 0, D(1), D(2), 1, -D(1)*Height, -D(2)*Height;]
+    b=[0; 0; Width; 0; Width; Height; 0; Height]
+    
+    % Matrix division Ax=b => x = A\b
+    x=matrix\b 
+    M=[x(1), x(2), x(3);
+        x(4), x(5), x(6);
+        x(7), x(8), 1
+        ] 
+end
+
+
+
+function  [ParamStr, yfitted] = FindTrace(X, Y, Width, Height)
+    %resize the data such that width is 1s and height is 2v
+    X=X./(Width)
+    Y=Y./(Height*0.5)-1
+    %plot(X, Y, 'ob'); DEBUG
+    %hold on;
+    %try plotting a sine
+    %to find frequency, approximate as a 12deg polyn
+    [Xsort, index]=sort(X)
+    Ysort=Y(index)
+    polyn = polyfit(Xsort,Ysort,12);
+    %find the points where the polyn is mean(Y)
+    polyn(end)=polyn(end)-mean(Y) %to solve polym=mean(Y)
+    sols=roots(polyn)
+    Xmax=max(X);
+    sols=sols(find(0<sols & sols<Xmax & conj(sols)==sols)) %check if solutions are in domain and real
+    if length(sols)<3 & 1<length(sols)
+        dist=sols(end)-sols(end-1)
+    elseif length(sols)==1 %this is probably gonna be a polynomial
+            dist=max(X)-min(X)
+    else dist=sols(end-1)-sols(end-2)
+    end
+
+    opts = fitoptions('Method','NonlinearLeastSquares');
+    ft=fittype('a*sin(b*x+c)+d');
+    [sortY, index]=sort(Y, 'descend');
+    omega=dist
+    opts.StartPoint=[abs((max(Y)-min(Y))/2), pi/abs(omega), Y(1), mean(Y)]
+    [fitResult, gofsine, output] = fit(X, Y, ft, opts );
+    %plot(fitResult); DEBUG
+    %try plotting a 4 degree polynomial
+    [polyn4, gofpolyn] = fit(Xsort,Ysort,'poly4');
+    %check the gofs and pick the lowest one
+    if gofpolyn.rmse< gofsine.rmse
+        %output polynomial coefficients
+        ParamStr=sprintf("a4: %.2f, a3: %.2f, a2: %.2f, a1: %.2f, RMSE: %.2f", polyn4(1), polyn4(2), polyn4(3), polyn4(4), gofpolyn.rmse)
+        yfitted=polyn4(X);
+    else
+        %output afpo of sine in paramstr
+        ParamStr=sprintf("A: %.2f[v], DC: %.2f[v], F: %.2f[Hz], P: %.2f [deg], RMSE: %.2f", fitResult.a, fitResult.d, fitResult.b/(2*pi), rad2deg(fitResult.c), gofsine.rmse)
+        yfitted=fitResult(X);
+    end
+end
+
+
+%function to order corners
+function [A,B,C,D] = orderCorners(Corners)
+%must find corner A by sorting the Corners over the smallest distance to
+%0,0, that is to say smallest x2+y2
+distances=Corners(:,1).^2 + Corners(:,2).^2
+[distances, index_distances]=sort(distances)
+xA=Corners(index_distances(1), 1);
+yA=Corners(index_distances(1), 2);
+A=[xA, yA];
+%put A at the beginning of Corners
+aux=[Corners(1, 1), Corners(1, 2)]
+Corners(index_distances(1), :)=aux
+Corners(1, :)=A
+%now we must order the B C D such that atan((y-yA)/(x-xA)) is minimal
+angles=atan2((Corners(:, 2)-yA),(Corners(:, 1)-xA))
+angles(1, :)=[] %delete A
+%we copy corners and we delete A from it as well
+CornersCopy=Corners;
+CornersCopy(1, :)=[] %delete A
+[angles, index_angles]=sort(angles)
+%now the last 3 angles correspond to B C and D
+B=[CornersCopy(index_angles(1), 1), CornersCopy(index_angles(1), 2)];
+C=[CornersCopy(index_angles(2), 1), CornersCopy(index_angles(2), 2)];
+D=[CornersCopy(index_angles(3), 1), CornersCopy(index_angles(3), 2)];
+end
+   
